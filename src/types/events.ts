b0181a560970ce268d28215405157cf150377efa
obrucheoghene <obrucheoghene1@gmail.com>
2025-08-05 08:32:50 +0100

    feat: add events

---
+++ src/types/events.ts
@@ -0,0 +1,9 @@
+export enum SignallingEvents {
+  Connected = 'connected',
+  JoinRoom = 'join-room',
+  JoinLobby = 'join-lobby',
+  JoinWaiters = 'join-waiters',
+}
+export enum PubSubEvents {
+  'EndMeeting' = 'end-meeting',
+}
