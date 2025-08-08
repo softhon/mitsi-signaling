a36626f20cb1cf0c0ea7a05cb858784fd12f0d60
obrucheoghene <obrucheoghene1@gmail.com>
2025-08-08 10:30:56 +0100

    implement create room

--- src/services/room.ts
+++ src/services/room.ts
@@ -1,5 +1,7 @@
 import EventEmitter from 'events';
 import Peer from './peer';
+import { redisServer } from '../servers/redis-server';
+import { RoomData, RoomInstanceData } from '../types/interfaces';
 
 class Room extends EventEmitter {
   roomId: string;
@@ -7,13 +9,13 @@ class Room extends EventEmitter {
   coHostEmails: string[];
   started: number;
   maxDuration: number;
-  maxPeers: number; //
+  maxPeers: number;
   allowRecording: boolean;
   allowWaiting: boolean;
   timeLeft: number;
 
-  selfDestructTimeout: NodeJS.Timeout | null;
-  endCountDownInterval: NodeJS.Timeout | null;
+  selfDestructTimeout: NodeJS.Timeout | undefined;
+  endCountDownInterval: NodeJS.Timeout | undefined;
 
   private peers: Map<string, Peer>;
   recorder: Peer | null;
@@ -55,11 +57,90 @@ class Room extends EventEmitter {
     );
     this.peers = new Map();
 
-    this.selfDestructTimeout = null;
-    this.endCountDownInterval = null;
+    this.selfDestructTimeout = undefined;
+    this.endCountDownInterval = undefined;
 
     this.recorder = null;
-    this.closed = true;
+    this.closed = false;
+
+    Room.rooms.set(roomId, this);
+  }
+
+  async close(end?: boolean): Promise<void> {
+    if (this.closed) return;
+    if (end) console.log('End meeting');
+
+    for (const peer of this.peers.values()) {
+      peer.close();
+    }
+
+    clearInterval(this.selfDestructTimeout);
+    clearTimeout(this.endCountDownInterval);
+
+    this.removeAllListeners();
+  }
+
+  static async create(roomId: string): Promise<Room> {
+    try {
+      // get room from redis if ongoing
+      const activeRoom = await redisServer.getValue(`room:${roomId}`);
+
+      let roomInstanceData: RoomInstanceData;
+
+      if (activeRoom) {
+        roomInstanceData = JSON.parse(activeRoom);
+      } else {
+        // get room data from api
+
+        // use domi data for now
+        const roomData: RoomData = {
+          id: crypto.randomUUID(),
+          title: 'Hello Room',
+          roomId: 'rty-rex-rrt',
+          host: {
+            id: crypto.randomUUID(),
+            name: 'Favour Grace',
+          },
+          coHostEmails: [],
+          guestEmails: [],
+          allowWaiting: false,
+        };
+
+        roomInstanceData = {
+          roomId: roomData.roomId,
+          hostId: roomData.host.id,
+          coHostEmails: roomData.coHostEmails,
+          started: Date.now(),
+          maxPeers: 50,
+          maxDuration: 180, // minutes
+          allowRecording: false,
+          allowWaiting: false,
+          recording: false,
+        };
+      }
+      const room = new Room(roomInstanceData);
+      await redisServer.setValue(
+        `room:${roomId}`,
+        JSON.stringify(roomInstanceData),
+        { NX: true }
+      );
+
+      return room;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  addPeer(peer: Peer): void {
+    this.peers.set(peer.id, peer);
+  }
+
+  getPeer(peerId: string): Peer | undefined {
+    return this.peers.get(peerId);
+  }
+  removePeer(peerId: string): void {
+    this.peers.delete(peerId);
   }
 }
 
