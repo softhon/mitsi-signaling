72cc00303793c71ac8f393065a4bc4d533567fb5
obrucheoghene <obrucheoghene1@gmail.com>
2025-08-16 22:09:28 +0100

    rename utils to lib

--- src/utils/helpers.ts
+++
@@ -1,8 +0,0 @@
-export const getKey = {
-  room: (roomId: string): string => `room-${roomId}`,
-  lobby: (roomId: string): string => `lobby-${roomId}`,
-  roomPeers: (roomId: string): string => `room-${roomId}-peers`,
-  roomPeerIds: (roomId: string): string => `room-${roomId}-peerids`,
-  roomActiveSpeakerPeerId: (roomId: string): string =>
-    `room-${roomId}-active-speaker-peerid`,
-};
