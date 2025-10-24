88e574b870c97bdfa7676ea356d3b8dbf77f4c78
obrucheoghene <obrucheoghene1@gmail.com>
2025-10-24 11:58:56 +0100

    remove unneccessary comments

--- src/services/medianode.ts
+++ src/services/medianode.ts
@@ -250,18 +250,6 @@ class MediaNode extends EventEmitter {
     this.grpcClient = new protoDescriptor.mediaSignalingPackage.MediaSignaling(
       `${this.ip}:${this.grpcPort}`,
       grpc.credentials.createInsecure()
-      // {
-      //   'grpc.keepalive_time_ms': 10000,
-      //   'grpc.keepalive_timeout_ms': 5000,
-      //   'grpc.keepalive_permit_without_calls': 1,
-      //   'grpc.http2.max_pings_without_data': 0,
-      //   'grpc.http2.min_time_between_pings_ms': 10000,
-      //   'grpc.http2.min_ping_interval_without_data_ms': 300000,
-      //   'grpc.max_receive_message_length': 4 * 1024 * 1024, // 4MB
-      //   'grpc.max_send_message_length': 4 * 1024 * 1024, // 4MB
-      //   'grpc.initial_reconnect_backoff_ms': 1000,
-      //   'grpc.max_reconnect_backoff_ms': 10000,
-      // }
     );
 
     // Wait for client to be ready
